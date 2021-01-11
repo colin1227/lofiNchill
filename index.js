2a3f72e8c80c00c27ec2c9cf09058b4befbceb79
Colin Daniel <colin.daniel1227@gmail.com>
2021-01-11 04:44:25 -0600

    build command

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes/index");
+const serverless = require("serverless-http");
+
 const port = 3000;
 
 const app = express();
@@ -11,3 +13,5 @@ app.use("/", routes);
 app.listen(port, () =>{
   console.log("Live.");
 })
+
+module.exports.handler = serverless(app);
