52c019fcc7a986f1c7a896623d4ee05ab508f5dd
Colin Daniel <colin.daniel1227@gmail.com>
2021-01-11 03:57:55 -0600

    cors?

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes/index");
 const port = 3000;
 
 const app = express();
+
+app.use(cors({ origin: "*" }))
 app.use("/", routes);
 
 app.listen(port, () =>{
